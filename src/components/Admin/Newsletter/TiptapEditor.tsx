827004004ea1536b41507ab1e9a2ef4e04e5c467
sheikmagizh12 <sheik@mntfuture.com>
2026-04-10 15:14:48 +0530

    feat: implement Tiptap rich text editor component with toolbar and formatting extensions

--- src/components/Admin/Newsletter/TiptapEditor.tsx
+++ src/components/Admin/Newsletter/TiptapEditor.tsx
@@ -4,7 +4,7 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Underline from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
-import TextStyle from '@tiptap/extension-text-style';
+import { TextStyle } from '@tiptap/extension-text-style';
 import Color from '@tiptap/extension-color';
 import Link from '@tiptap/extension-link';
 import {
